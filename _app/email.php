3fba2f14b72184e1d00d576cad2fad82fbbda227
Lucas Sousa Rosa <lucassr.job@gmail.com>
2023-02-14 14:29:24 -0300

    update 2

--- _app/email.php
+++ _app/email.php
@@ -1,49 +1,43 @@
 <?php
 
-namespace Notification\Email;
+
+namespace Notification;
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 
 class Email
 {
     private $mail = \stdClass::class;
 
-    public function __construct($smtpDebug, $host, $user, $password, $smtpSecure, $port, $setFromEmail, $setFromName)
-    {
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName) {
         $this->mail = new PHPMailer(true);
-        //Server settings
-        $this->mail->SMTPDebug = $smtpDebug; //SMTP::DEBUG_SERVER;        // Enable verbose debug output
-        $this->mail->isSMTP();                                            // Send using SMTP
-        $this->mail->Host = $host; //'smtp.gmail.com';                    // Set the SMTP server to send through
-        $this->mail->SMTPAuth = true;                                     // Enable SMTP authentication
-        $this->mail->Username = $user; //'developerfsphp@gmail.com';      // SMTP username
-        $this->mail->Password = $password; //'9cPA8!Vhd3H*njBn';          // SMTP password
-        $this->mail->SMTPSecure = $smtpSecure; //'tls';                   // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $this->mail->Port = $port; //587;                                 // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-        $this->mail->CharSet = "utf-8";
-        $this->mail->setLanguage("br");
+        $this->mail->SMTPDebug  = $smtpDebug;      //Enable verbose debug output
+        $this->mail->isSMTP();                     //Send using SMTP
+        $this->mail->Host       = $host;           //Set the SMTP server to send through
+        $this->mail->SMTPAuth   = true;            //Enable SMTP authentication
+        $this->mail->Username   = $user;           //SMTP username
+        $this->mail->Password   = $pass;           //SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;     //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $this->mail->Port       = $port;           //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $this->mail->CharSet = 'utf-8';
+        $this->mail->setLanguage('br');
         $this->mail->isHTML(true);
-
-        //Recipients
-        //$this->mail->setFrom('developerfsphp@gmail.com', 'Team Developer');
         $this->mail->setFrom($setFromEmail, $setFromName);
     }
 
-    public function send($subject, $body, $replyEmail, $replyName, $addressEmail, $addressName)
+    public function sendMail($subject, $body, $replayEmail, $replayName, $addressEmail, $addressName)
     {
         $this->mail->Subject = (string)$subject;
         $this->mail->Body = $body;
 
-        $this->mail->addReplyTo($replyEmail, $replyName);
+        $this->mail->addReplyTo($replayEmail, $replayName);
         $this->mail->addAddress($addressEmail, $addressName);
 
         try {
             $this->mail->send();
-        } catch (Exception $exception) {
-            echo "Erro ao enviar e-mail: {$this->mail->ErrorInfo} {$exception->getMessage()}";
+        } catch(Exception $e) {
+            echo "Erro ao enviar o e-mail: {$this->mail->ErrorInfo} {$e->getMessage()}";
         }
     }
 }
