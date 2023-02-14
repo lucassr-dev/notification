50a4e995fb1e5cba8fa4cc09d5a00ede00138e80
Lucas Sousa Rosa <lucassr.job@gmail.com>
2023-02-14 14:23:15 -0300

    update

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . "lib_ext/autoload.php";
+
+use Notification;
+
+$email = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls/ssl)", "port (587)",
+    "from@email.com", "From Name");
+
+$email->sendEmail("SUbject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
+
+var_dump($email);
